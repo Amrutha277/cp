40bd792dc45f9e2645be178274e3247831ae3ff5
Amrutha277 <161395362+Amrutha277@users.noreply.github.com>
2025-01-28 21:27:39 -0500

    Update file 1/28/2025, 9:27:51 PM

--- 1922-CountGoodNumbers/1922-CountGoodNumbers.py
+++ 1922-CountGoodNumbers/1922-CountGoodNumbers.py
@@ -1,33 +1,17 @@
 class Solution(object):
     def countGoodNumbers(self, n):
+        
         """
         :type n: int
         :rtype: int
         """
         MOD = 10**9 + 7
-        if n==0:
-            return 0
+        ans = 1
+        rem = n % 2
+        n -= rem
+        ans = pow(20, n // 2, MOD)
         
-        if n==1:
-            return 5
+        if rem == 1:
+            ans = (ans * 5) % MOD
         
-        even_count=5
-        prime_count=4
-        
-        even_pos= (n+1)//2
-        odd_pos = n//2
-
-        def modular_exponentiation(base, exp, mod):
-            result = 1
-            while exp > 0:
-                if exp % 2 == 1:
-                    result = (result * base) % mod
-                base = (base * base) % mod
-                exp //= 2
-            return result
-     
-        even_power = modular_exponentiation(even_count, even_pos, MOD)
-        prime_power = modular_exponentiation(prime_count, odd_pos, MOD)
-        
-        return (even_power * prime_power) % MOD
-
+        return ans
