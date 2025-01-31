4c5296fffd91a51e7f886a091a8e6005fb902102
Amrutha277 <161395362+Amrutha277@users.noreply.github.com>
2025-01-31 13:14:15 -0500

    Update file 1/31/2025, 1:14:27 PM

--- 678-ValidParenthesisString/678-ValidParenthesisString.py
+++ 678-ValidParenthesisString/678-ValidParenthesisString.py
@@ -4,19 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        low, high=0,0
-        for i in range(len(s)):
-            if s[i]=="(":
-                low+=1
-                high+=1
-            elif s[i]==")":
-                low= max(0,low-1)
-                high-=1
-            elif s[i]=="*":
-                low=max(0,low-1)
-                high+=1
-            if high<0:
-                return False
-        return low==0
+        left_bal, right_bal=0,0
+        for char in s:
+            if char == '(' or char == '*':
+                left_bal += 1
+            else:
+                left_bal -= 1
+            if left_bal < 0:
+                return False  # Too many ')'
+
         
+        for char in reversed(s):
+            if char == ')' or char == '*':
+                right_bal += 1
+            else:
+                right_bal -= 1
+            if right_bal < 0:
+                return False  # Too many '('
+
+        return True
 
