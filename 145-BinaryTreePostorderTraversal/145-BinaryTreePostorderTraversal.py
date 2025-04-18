f24ceaafe10227c9e192aa9440a3737af351cb0f
Amrutha277 <161395362+Amrutha277@users.noreply.github.com>
2025-04-18 14:09:00 -0400

    File updated at 4/18/2025, 2:09:37 PM

--- 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.py
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.py
@@ -1,3 +1,4 @@
+# Last updated: 4/18/2025, 2:09:37 PM
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -11,12 +12,16 @@ class Solution(object):
         :rtype: List[int]
         """
         result=[]
-        def traverse(node):
-            if not node:
-                return
-            traverse(node.left)
-            traverse(node.right)
-            result.append(node.val)
-        traverse(root)
-        return result
+        if not root:
+            return []
+        s1=[root]
+        s2=[]
+        while s1:
+            node= s1.pop()
+            s2.append(node)
+            if node.left:
+                s1.append(node.left)
+            if node.right:
+                s1.append(node.right)
+        return[node.val for node in reversed(s2)]
         
