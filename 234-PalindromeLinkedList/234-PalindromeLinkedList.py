1d821c5819c4f2781b3ba7149a37480ab6f1d98a
Amrutha277 <161395362+Amrutha277@users.noreply.github.com>
2025-07-05 20:04:03 -0400

    File updated at 7/5/2025, 8:05:12 PM

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.py
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.py
@@ -1,4 +1,4 @@
-# Last updated: 7/5/2025, 7:39:46 PM
+# Last updated: 7/5/2025, 8:05:12 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,17 +6,39 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        nodeslist= []
-        cur= head
-        while cur:
-            nodeslist.append(cur.val)
-            cur= cur.next
+        # nodeslist= []
+        # cur= head
+        # while cur:
+        #     nodeslist.append(cur.val)
+        #     cur= cur.next
         
-        left, right= 0, len(nodeslist)-1
-        while left<right:
-            if nodeslist[left] != nodeslist[right]:
+        # left, right= 0, len(nodeslist)-1
+        # while left<right:
+        #     if nodeslist[left] != nodeslist[right]:
+        #         return False
+        #     left+=1
+        #     right-=1
+        # return True
+
+        slow, fast= head, head
+        while fast and fast.next:
+            slow= slow.next
+            fast= fast.next.next
+        prev= None
+        
+        while slow:
+            temp= slow.next
+            slow.next= prev
+            prev= slow
+            slow= temp
+
+        left, right= head, prev
+        while right:
+            if left.val != right.val:
                 return False
-            left+=1
-            right-=1
-        return True
+            left= left.next
+            right= right.next
+        return True 
+
+
 
