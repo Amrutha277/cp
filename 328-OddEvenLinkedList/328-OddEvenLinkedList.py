3acb664592c21b40410b0d6a11df9674160d11a9
Amrutha277 <161395362+Amrutha277@users.noreply.github.com>
2025-07-05 20:28:15 -0400

    File updated at 7/5/2025, 8:29:24 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.py
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.py
@@ -1,27 +1,24 @@
+# Last updated: 7/5/2025, 8:29:24 PM
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def oddEvenList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-        
-        odd = head
-        even = head.next
-        even_head = even
+        odd= head
+        even= head.next
+        even_og= even
 
         while even and even.next:
-            odd.next = even.next
-            odd = odd.next
-            even.next = odd.next
-            even = even.next
-        
-        odd.next = even_head
+            odd.next= even.next
+            odd= odd.next
+
+            even.next= odd.next
+            even= even.next
+
+        odd.next= even_og
+
         return head
-        
