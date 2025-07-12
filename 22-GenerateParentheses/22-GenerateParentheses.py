6777ea1edcfce29dea8fa8d346a690fac714dafd
Amrutha277 <161395362+Amrutha277@users.noreply.github.com>
2025-07-12 15:45:56 -0400

    File updated at 7/12/2025, 3:47:07 PM

--- 22-GenerateParentheses/22-GenerateParentheses.py
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -1,25 +1,23 @@
-class Solution(object):
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-      
-        def backtrack(s, left, right):
-            if len(s) == 2*n:
-                result.append(s)
-                return
-            if left<n:
-                backtrack(s+"(",left+1,right)
-            if right<left:
-                backtrack(s+")",left, right+1)
-        
-
-        
+# Last updated: 7/12/2025, 3:47:07 PM
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
         result=[]
+        if n==0:
+            return []
+        left=0
+        right=0
+        def backtrack(current, left, right):
+            if len(current) == 2*n:
+                result.append(current)
+                return
+            
+            if left<n:
+                backtrack(current+'(',left+1, right)
+            
+            if left>right:
+                # print("ji")
+                backtrack(current+')', left, right+1)
+            # print(current)
+            
         backtrack("",0,0)
         return result
-            
-
-        
-        
