6b64c2df00968613c50dc7244c0ce594d25ec0c7
Amrutha277 <161395362+Amrutha277@users.noreply.github.com>
2025-07-12 18:28:34 -0400

    File updated at 7/12/2025, 6:29:45 PM

--- 216-CombinationSumIII/216-CombinationSumIII.py
+++ 216-CombinationSumIII/216-CombinationSumIII.py
@@ -1,18 +1,24 @@
-# Last updated: 7/12/2025, 6:27:16 PM
+# Last updated: 7/12/2025, 6:29:45 PM
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        result=[]
-        
-        def backtrack(index, path, cur_sum):
-            if len(path) == k:
-                if cur_sum == n:
-                    result.append(path[:])
+        results = []
+        def solve(ind, total, subset):
+            if total == 0 and len(subset) == k:
+                results.append(subset.copy())
                 return
-
-            for i in range(index, 10):
-                path.append(i)
-                backtrack(i+1, path, cur_sum+i)
-                path.pop()
-
-        backtrack(1,[],0)
-        return result
+            
+            if total < 0 :
+                return
+            
+            if ind > 9 :
+                return
+            
+            if len(subset) > k :
+                return
+            
+            for i in range(ind, 10):
+                subset.append(i)
+                solve(i+1, total - i, subset)
+                subset.pop()
+        solve(1, n, [])
+        return results
