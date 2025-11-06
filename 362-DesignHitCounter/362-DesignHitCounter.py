606b2ca08fa9308c512b985d710ea1682a39d075
Amrutha277 <161395362+Amrutha277@users.noreply.github.com>
2025-11-05 19:58:56 -0500

    File updated at 11/5/2025, 7:58:55 PM

--- 362-DesignHitCounter/362-DesignHitCounter.py
+++ 362-DesignHitCounter/362-DesignHitCounter.py
@@ -1,4 +1,4 @@
-# Last updated: 11/5/2025, 7:54:46 PM
+# Last updated: 11/5/2025, 7:58:55 PM
 class HitCounter:
 
     def __init__(self):
@@ -6,12 +6,17 @@ class HitCounter:
         self.total=0
 
     def hit(self, timestamp: int) -> None:
-        self.hits.append(timestamp)
+        if self.hits and self.hits[-1][0] == timestamp:
+            self.hits[-1][1] +=1
+        else:
+            self.hits.append([timestamp,1])
+        self.total+=1
 
     def getHits(self, timestamp: int) -> int:
-        while self.hits and self.hits[0] <= timestamp-300:
-            self.hits.popleft()
-        return len(self.hits)
+        while self.hits and self.hits[0][0] <= timestamp-300:
+            old_timestamp, count = self.hits.popleft()
+            self.total -= count
+        return self.total
         
 
 
