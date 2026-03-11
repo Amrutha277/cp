3ff68beb5b2101ebc736520656c139f83c208661
Amrutha277 <161395362+Amrutha277@users.noreply.github.com>
2026-03-10 20:37:20 -0400

    File updated at 3/10/2026, 8:37:20 PM

--- 1802-MaximumValueataGivenIndexinaBoundedArray/1802-MaximumValueataGivenIndexinaBoundedArray.py
+++ 1802-MaximumValueataGivenIndexinaBoundedArray/1802-MaximumValueataGivenIndexinaBoundedArray.py
@@ -1,31 +1,25 @@
-# Last updated: 3/10/2026, 8:01:51 PM
+# Last updated: 3/10/2026, 8:37:20 PM
 1class Solution:
 2    def maxValue(self, n: int, index: int, maxSum: int) -> int:
-3        def side_sum(peak:int, count:int) -> int:
-4            start = peak-1
-5            if start>= count:
-6                return (start + (start-count+1))*count//2
-7            else:
-8                decreasing_part = start*(start+1)//2
-9                ones_part = count-start
-10                return decreasing_part+ ones_part
-11        
-12        def can_make(value:int)->bool:
-13            left_count= index
-14            right_count= n-index-1
-15            total= side_sum(value, left_count)+value+side_sum(value, right_count)
-16            return total<=maxSum
-17        
-18        left=1
-19        right=maxSum
-20        answer = 1
-21
-22        while left<=right:
-23            mid= (left+right)//2
-24
-25            if can_make(mid):
-26                answer= mid
-27                left= mid+1
-28            else:
-29                right= mid-1
-30        return answer
+3        def get_sum(h,length):
+4            if length ==0:
+5                return 0
+6            if h>=length:
+7                return (h+h-length+1)*length //2
+8            else:
+9                return (h+1)*h//2 + (length-h)
+10        
+11        left, right= 1, maxSum
+12        ans=1
+13
+14        while left<=right:
+15            mid= (left+right)//2
+16
+17            total_sum= mid+ get_sum(mid-1, index) + get_sum(mid-1, n-1-index)
+18
+19            if total_sum <= maxSum:
+20                ans= mid
+21                left = mid+1
+22            else:
+23                right= mid-1
+24        return ans
